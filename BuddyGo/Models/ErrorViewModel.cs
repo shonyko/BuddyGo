8b86ab3f6ba97840b426300f954ebe2af546b71e
shonyko <alexkovacs@rocketmail.com>
2022-03-10 11:41:10 +0200

    Add project files.

---
+++ BuddyGo/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace BuddyGo.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
