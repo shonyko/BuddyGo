70997190416fabe8644a08bc7fd9206fa48609a6
shonyko <alexkovacs@rocketmail.com>
2022-03-30 23:11:43 +0300

    Added Vuetify & Axios

--- client-app/src/main.js
+++ client-app/src/main.js
@@ -1,15 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  vuetify,
+
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
